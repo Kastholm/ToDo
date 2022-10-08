392ae386ccf35d6b85bec737f8e9273e3ae72ce3
Kastholm <Kastholm1995@gmail.com>
2022-10-08 14:47:50 +0200

    opdeling ad bokse

--- scripts.js
+++ scripts.js
@@ -71,37 +71,43 @@ document.querySelector(".checkboxes").addEventListener("click", () => {
   remindCB.style.pointerEvents = "visible";
   todoCB.style.pointerEvents = "visible";
 });
-
+let boxVariant = "";
 add.addEventListener("click", () => {
   if (buyCB.checked) {
+    boxVariant = "buy";
     const addBuyBox = document.createElement("div");
     addBuyBox.className = "buy-box box";
     document.querySelector(".buy-container").appendChild(addBuyBox);
     addBuyBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${header.value}</h2>
-          <h3>${desc.value}</h3>
+          <h3 class='description'>${desc.value}</h3>
           <p>${link.value}</p>
           <h4 class="date">Tilføjet ${day}-${month} | Kl. ${h}:${m}</h4>`;
     trashItem();
+    localAdd();
     return;
   } else if (remindCB.checked) {
+    boxVariant = "remind";
     const addRemindBox = document.createElement("div");
     addRemindBox.className = "remind-box box";
     document.querySelector(".remind-container").appendChild(addRemindBox);
     addRemindBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${header.value}</h2>
-          <h3>${desc.value}</h3>
+          <h3 class='description'>${desc.value}</h3>
           <p>${link.value}</p>
           <h4 class="date">Tilføjet ${day}-${month} | Kl. ${h}:${m}</h4>`;
     trashItem();
+    localAdd();
     return;
   } else if (todoCB.checked) {
+    boxVariant = "ToDo";
     const addTodoBox = document.createElement("div");
     addTodoBox.className = "todo-box box";
     document.querySelector(".todo-container").appendChild(addTodoBox);
     addTodoBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${header.value}</h2>
-          <h3>${desc.value}</h3>
+          <h3 class='description'>${desc.value}</h3>
           <p>${link.value}</p>
           <h4 class="date">Tilføjet ${day}-${month} | Kl. ${h}:${m}</h4>`;
     trashItem();
+    localAdd();
     return;
   }
 });
@@ -116,32 +122,128 @@ function trashItem() {
   for (let i = 0, b = 0; i < trash.length, b < boxes.length; i++, b++) {
     trash[i].addEventListener("click", () => {
       boxes[b].remove();
+      console.log(boxes[b]);
+      let historyArray = [];
+      let historyJSON = localStorage.getItem("history");
+      historyArray = JSON.parse(historyJSON);
+      historyArray.pop(historyArray[b]);
+      localStorage.setItem("history", JSON.stringify(historyArray));
+
+      console.log(historyArray);
     });
   }
 }
 
-/* let CB = document.querySelectorAll('.checkboxes input')
-CB.forEach((box, index) => {
-     
-}); */
-/* 
- next.addEventListener("click", () => {
-     x++;
-   
-     if (x > 2) {
-       x = 0;
-     }
-   
-     fields.forEach((box, index) => {
-       if (index === x) {
-         box.style.display = "block";
-       } else {
-         box.style.display = "none";
-       }
-     });
-
-   }); */
-
 document.querySelector(".new").addEventListener("click", () => {
   inputC.style.display = "grid";
+});
+
+function localAdd() {
+  const headerA = document.querySelectorAll(".header");
+  const descA = document.querySelectorAll(".description");
+
+  /* for (
+    let num = 0, numT = 0;
+    num < header.length, numT < desc.length;
+    num++, numT++
+  ) {} */
+  // makes the historry array in the scope
+  let historyArray = [];
+  let historyJSON = localStorage.getItem("history");
+  // if exists append player or add to points
+  if (historyJSON) {
+    historyArray = JSON.parse(historyJSON);
+    let historikSpiller = historyArray.find(
+      (x) => x["header"] == headerA[0].value
+    );
+    if (historikSpiller) {
+    } else {
+      historyArray.push({
+        header: headerA[0].value,
+        desc: descA[0].innerHTML,
+        wat: `${day}-${month} | Kl. ${h}:${m}`,
+        variant: boxVariant,
+      });
+    }
+  } else {
+    historyArray = [
+      {
+        variant: boxVariant,
+        header: headerA[0].value,
+        wat: `${day}-${month} | Kl. ${h}:${m}`,
+        desc: descA[0].innerHTML,
+      },
+    ];
+  }
+  // always saves here
+
+  localStorage.setItem("history", JSON.stringify(historyArray));
+}
+
+window.addEventListener("load", (event) => {
+  // gets the JSON from the storage
+  let historyJSON2 = localStorage.getItem("history");
+
+  if (historyJSON2) {
+    // if exsists makes into an array
+    let historyArray2 = JSON.parse(historyJSON2);
+    console.log(historyArray2);
+    for (let i in historyArray2) {
+      // write out the historic players
+      /* console.log(historyArray2[i].header);
+      console.log(historyArray2[i].desc);
+      console.log(historyArray2[i].variant); */
+      if (historyArray2[i].variant === "buy") {
+        const addBuyBox = document.createElement("div");
+        addBuyBox.className = "buy-box box";
+        document.querySelector(".buy-container").appendChild(addBuyBox);
+        addBuyBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${historyArray2[i].header}</h2>
+        <h3>${historyArray2[i].desc}</h3>
+        <p>${link.value}</p>
+        <h4 class="date">Tilføjet ${historyArray2[i].wat} </h4>`;
+      } else if (historyArray2[i].variant === "remind") {
+        const addRemindBox = document.createElement("div");
+        addRemindBox.className = "remind-box box";
+        document.querySelector(".remind-container").appendChild(addRemindBox);
+        addRemindBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${historyArray2[i].header}</h2>
+                 <h3>${historyArray2[i].desc}</h3>
+                 <p>${link.value}</p>
+                 <h4 class="date">Tilføjet ${historyArray2[i].wat} </h4>`;
+      } else if (historyArray2[i].variant === "ToDo") {
+        const addTodoBox = document.createElement("div");
+        addTodoBox.className = "todo-box box";
+        document.querySelector(".todo-container").appendChild(addTodoBox);
+        addTodoBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${historyArray2[i].header}</h2>
+    <h3>${historyArray2[i].desc}</h3>
+    <p>${link.value}</p>
+    <h4 class="date">Tilføjet ${historyArray2[i].wat} </h4>`;
+      }
+
+      /* if ((historyArray2[i].variant = "remind")) {
+               console.log("remind");
+             } */
+      //
+      /* const addRemindBox = document.createElement("div");
+      addRemindBox.className = "remind-box box";
+      document.querySelector(".remind-container").appendChild(addRemindBox);
+      addRemindBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${historyArray2[i].header}</h2>
+          <h3>${historyArray2[i].desc}</h3>
+          <p>${link.value}</p>
+          <h4 class="date">Tilføjet ${historyArray2[i].wat} </h4>`; */
+      //
+      /* const addRemindBox = document.createElement("div");
+      addRemindBox.className = "remind-box box";
+      document.querySelector(".remind-container").appendChild(addRemindBox);
+      addRemindBox.innerHTML = `<i class="fa-solid fa-trash"></i><h2>${historyArray2[i].header}</h2>
+          <h3>${historyArray2[i].desc}</h3>
+          <p>${link.value}</p>
+          <h4 class="date">Tilføjet ${historyArray2[i].wat} </h4>`; */
+      trashItem();
+    }
+  }
+  /* ${day}-${month} | Kl. ${h}:${m} */
+
+  /* if(boxes[b].classList.contains('remind-box')){ */
+
+  /* } */
 });
